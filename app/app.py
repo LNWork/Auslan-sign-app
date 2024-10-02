b5136f0ef9610e84913cd8a9a37fabe172be7373
LNWork <luca.neal@deckard.com>
2024-10-02 15:35:12 +1000

    point before adjusting output results

--- app/app.py
+++ app/app.py
@@ -7,17 +7,19 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('keypoints.html')
+
+@app.route('/sign_to_text')
+def run_text_to_sign():
+    return render_template('text_to_sign.html')
  
 @app.route('/api/keypoints', methods=['POST'])
 def receive_keypoints():
     data = request.json  # Get the JSON data from the request
-    keypoints = data.get("poseLandmarks")  # Extract keypoints from the received data
-
+    # keypoints = data.get("keypoints")  # Extract keypoints from the received data
     # You can process, store, or log the keypoints here
-    print("Received landmarks:", keypoints)  # Print keypoints for demonstration
+    print("Received landmarks:", data)  # Print keypoints for demonstration
 
-    # Respond with a success message
-    return jsonify({"message": "Landmarks received successfully!"})
-
+    return jsonify({"message": "Keypoints received successfully!"})
 if __name__ == "__main__":
     app.run(debug=True)
+
