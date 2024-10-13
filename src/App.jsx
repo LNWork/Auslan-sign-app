bc84c1440627705b4d182de16d2a24f75582f90e
Albert Nguyen <albertnguyen2002@hotmail.com>
2024-10-13 19:15:20 +1100

    Package and chunk update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import './App.css'
-import VideoInput from './components/VideoInput'
 import TranslateApp from './pages/Translate'
 
 const App = () => {
     return(
         <div>
             <h1 className="text-3xl font-bold underline">
-                Translate Auslan!
+                AuslanLive!
             </h1>
-            {/* <VideoInput /> */}
             <TranslateApp />
         </div>
     )
