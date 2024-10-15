fbfe91859141bf21445891b7f69c6cbc42f399ff
james.mcmahon <james.mcmahon@racebookhq.com.au>
2024-10-15 12:15:16 +1100

    Logging Added for Flask App (Results Parser) & Server Accessibility Increased

--- app/app.py
+++ app/app.py
@@ -1,11 +1,18 @@
 from flask import Flask, request, jsonify, render_template
 from results_parser import ResultsParser
-
+import logging
 from markupsafe import escape
 
 app = Flask(__name__)
 
 results_parser = ResultsParser()
+
+# Set up logging
+logging.basicConfig(
+    filename='app.log',  # Log file name
+    level=logging.INFO,   # Set the logging level
+    format='%(asctime)s - %(levelname)s - %(message)s'  # Log message format
+)
 
 @app.route("/")
 def index():
@@ -17,12 +24,18 @@ def run_text_to_sign():
 
 @app.route('/model_output', methods=['GET', 'POST'])
 def model_output_parse():
-    model_output = request.get_json()
-    model_output_data = model_output['model_output']
-    print("Received Model Output:", model_output)
-    processed_output = results_parser.parse_model_output(model_output_data)
 
-    return jsonify({"message": processed_output}), 200
+    try:
+        logging.info('Received request on /model_output' + model_output)
+        model_output = request.get_json()
+        model_output_data = model_output['model_output']
+        print("Received Model Output:", model_output)
+        processed_output = results_parser.parse_model_output(model_output_data)
+        logging.info('Model Output Processed Successfully! Message:' + processed_output)
+        return jsonify({"message": processed_output}), 200
+    except Exception as e:
+        logging.error(f'Error processing request: {e}')
+        return jsonify({"error": "Internal Server Error"}), 500
  
 @app.route('/api/keypoints', methods=['POST'])
 def receive_keypoints():
@@ -33,5 +46,5 @@ def receive_keypoints():
 
     return jsonify({"message": "Keypoints received successfully!"})
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
