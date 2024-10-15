d5f17938508397110c3ee041824369aa848d5153
james.mcmahon <james.mcmahon@racebookhq.com.au>
2024-10-15 12:33:52 +1100

    Logging fixed for Flask App, extra error handling

--- app/app.py
+++ app/app.py
@@ -8,11 +8,25 @@ app = Flask(__name__)
 results_parser = ResultsParser()
 
 # Set up logging
-logging.basicConfig(
-    filename='app.log',  # Log file name
-    level=logging.INFO,   # Set the logging level
-    format='%(asctime)s - %(levelname)s - %(message)s'  # Log message format
-)
+logger = logging.getLogger()  # Create a logger
+logger.setLevel(logging.INFO)  # Set the logging level
+
+# Create a file handler to log messages to a file
+file_handler = logging.FileHandler('app.log')
+file_handler.setLevel(logging.INFO)  # Set the file logging level
+
+# Create a console handler to log messages to the terminal
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)  # Set the console logging level
+
+# Create a formatter and set it for both handlers
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+# Add both handlers to the logger
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
 
 @app.route("/")
 def index():
@@ -24,27 +38,25 @@ def run_text_to_sign():
 
 @app.route('/model_output', methods=['GET', 'POST'])
 def model_output_parse():
-
     try:
-        logging.info('Received request on /model_output' + model_output)
+
         model_output = request.get_json()
+        logging.info('Received request on /model_output: %s', model_output)  # Updated log message
         model_output_data = model_output['model_output']
-        print("Received Model Output:", model_output)
         processed_output = results_parser.parse_model_output(model_output_data)
-        logging.info('Model Output Processed Successfully! Message:' + processed_output)
+    
+        logging.info('Model Output Processed Successfully! Message: %s', processed_output)  # Updated log message
         return jsonify({"message": processed_output}), 200
+
     except Exception as e:
         logging.error(f'Error processing request: {e}')
-        return jsonify({"error": "Internal Server Error"}), 500
- 
+        return jsonify({"error": "Internal Server Error. Check JSON Format"}), 500
+
 @app.route('/api/keypoints', methods=['POST'])
 def receive_keypoints():
     data = request.json  # Get the JSON data from the request
-    # keypoints = data.get("keypoints")  # Extract keypoints from the received data
-    # You can process, store, or log the keypoints here
-    print("Received landmarks:", data)  # Print keypoints for demonstration
-
+    logging.info("Received landmarks: %s", data)  # Log keypoints received
     return jsonify({"message": "Keypoints received successfully!"})
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
-
