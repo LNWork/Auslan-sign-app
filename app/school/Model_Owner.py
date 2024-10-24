615fbcc1271ec53034c024db08205c58ed4b0800
James-McMahonUTS <james.mcmahon@student.uts.edu.au>
2024-10-24 18:14:12 +1100

    Dw about this (I think)

--- app/school/Model_Owner.py
+++ app/school/Model_Owner.py
@@ -3,6 +3,23 @@ import pandas as pd
 from tensorflow.keras.models import *
 import json
 import numpy as np
+import tensorflow as tf
+import asyncio
+
+# Define a custom layer so that the model can load
+@tf.keras.utils.register_keras_serializable()
+class ExpandAxisLayer(tf.keras.layers.Layer):
+    def __init__(self, axis=1, **kwargs):
+        super(ExpandAxisLayer, self).__init__(**kwargs)
+        self.axis = axis
+
+    def call(self, inputs):
+        return tf.expand_dims(inputs, axis=self.axis)
+
+    def get_config(self):
+        config = super(ExpandAxisLayer, self).get_config()
+        config.update({"axis": self.axis})
+        return config
 
 class Model:
     def __init__(self, model_path):
@@ -10,21 +27,24 @@ class Model:
         self.model_path = model_path
 
         # Just remember that indexed are strings
-        self.outputs = json.load(open("word_index.txt"))
+        # Opening the classes
+        with open("class_label_index.json") as f:
+            self.outputs = json.load(f)
 
         # Create model here
         self.model = load_model(filepath=model_path)
 
-    def query_model(self, keypoints):
+    async def query_model(self, keypoints):
+        print("IN QUERY")
         # Format keypoints so it fits the model
         formatted_keypoints = self.__format_input_keypoints(keypoints)
 
         # Query the model
-        result = self.__get_model_result(formatted_keypoints)
+        result = await self.__get_model_result(formatted_keypoints)
 
         # Parse results so it fits the formate needed
         final_result = self.__format_model_results(result)
-
+        print("FORMATTE RESSULTS")
         return final_result
     
     ############################# Private helper functions needed for query model #############################
@@ -36,23 +56,22 @@ class Model:
 
         return keypoints
 
-    def __get_model_result(self, keypoints):
+    async def __get_model_result(self, keypoints):
+        print("CALLINGTHE MODEEL")
         # just query the model
-        return self.model.predict(keypoints)[0]
-
+        result = await asyncio.to_thread(self.model.predict, keypoints)
+        return result[0]
+    
     def __format_model_results(self, result):
         # Loop through resuklts and append the correct class to the probability
-
+        
         fomated_results = {"model_output": []}
         for i in range(len(result)):
-            #! REMOVE LATER BUT HERE CAUSE OF THE MISMATCH INDEX
-            if i > 606:
-                continue
-            
             # Adding the results
             str_index = str(i)
             fomated_results['model_output'].append([self.outputs[str_index], result[i]])
         
+        fomated_results['model_output'] = sorted(fomated_results['model_output'], key=lambda x: float(x[1]), reverse=True)[:10]
 
         return fomated_results
     ############################# Private helper functions needed for query model #############################
