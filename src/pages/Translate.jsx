a5df38e6ef89575332f57f20391be4a41c4b64fd
Cameron-Chilvers-UTS <Cameron.s.chilvers@student.uts.edu.au>
2024-10-25 23:30:37 +1100

    fix: fix1 fornt end sign to test

--- src/pages/Translate.jsx
+++ src/pages/Translate.jsx
@@ -19,6 +19,7 @@ const TranslateApp = () => {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': "*"
         },
       });
 
